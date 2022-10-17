61aaeff035187aa431b1365ed5650cd50d26f4d7
Tobias <spam@tobiaswittenburg.com>
2022-10-17 20:41:16 +0200

    Blazor Server to Blazor Client

--- BetterOOF/Program.cs
+++ BetterOOF/Program.cs
@@ -1,34 +1,14 @@
-using BetterOOF.Data;
-using BetterOOF.Services;
-using Microsoft.AspNetCore.Components;
+using BetterOOF;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using BetterOOF.Services;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<ClipboardService>();
 
-var app = builder.Build();
+await builder.Build().RunAsync();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
-
-
-
-app.Run();
