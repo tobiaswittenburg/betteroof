5f9b13c01f430484f102fa62a4d238f3bd1cb9db
Tobias <spam@tobiaswittenburg.com>
2022-11-14 15:41:48 +0100

    Migration to .NET 7.0

--- BetterOOF/Program.cs
+++ BetterOOF/Program.cs
@@ -10,5 +10,9 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<ClipboardService>();
 
+
+
+
+
 await builder.Build().RunAsync();
 
